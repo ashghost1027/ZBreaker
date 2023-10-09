c0fae2423717e18053332bf54ab415a53a9f2718
Aswin Paul <v.aswinpaul2004@gmail.com>
2023-10-09 14:46:50 +0530

    fixed bugs

--- motorbike.py
+++ motorbike.py
@@ -9,11 +9,14 @@ while total_vehicle_count > 0:
     for _ in range(i):
         row.append("bike")
         total_vehicle_count -= 1
+        if total_vehicle_count <= 0:
+            break
     
 for i in row[len(row)//2:]:
     if i == "bike":
         count_of_motorbikes_in_second_half += 1
 print(count_of_motorbikes_in_second_half)
 print(row)
+
     
 
